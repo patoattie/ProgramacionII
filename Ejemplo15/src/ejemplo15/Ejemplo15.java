1484aa52914a2e8cdb6742636e66296bf32db4dd
patoattie <patoattie@gmail.com>
2018-11-21 17:38:10 -0300

    Ejemplo clase 15. Polimorfismo

--- Ejemplo15/src/ejemplo15/Ejemplo15.java
+++ Ejemplo15/src/ejemplo15/Ejemplo15.java
@@ -17,7 +17,20 @@ public class Ejemplo15
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        ClaseUno uno = new ClaseUno("Atributo String Uno");
+        ClaseDos dos = new ClaseDos("Atributo String Dos", 2);
+        ClaseDos dos1 = new ClaseDos("Atributo String Tres", 2);
+        ClaseTres tres = new ClaseTres(dos1, 3.0F);
+        
+        System.out.println("1");
+        System.out.println(uno.getUno());
+        System.out.println(uno.mostrarUno());
+        System.out.println("2");
+        System.out.println(dos.getDos());
+        System.out.println(dos.mostrarDos());
+        System.out.println("3");
+        System.out.println(tres.getTres());
+        System.out.println(tres.mostrarTres());
     }
     
 }
