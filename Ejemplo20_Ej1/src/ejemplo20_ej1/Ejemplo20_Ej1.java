d374c90217a50e6ceb82468589305c6303837806
patoattie <patoattie@gmail.com>
2018-12-12 16:39:28 -0300

    Ejercicio 1 clase 20. Excepciones

---
+++ Ejemplo20_Ej1/src/ejemplo20_ej1/Ejemplo20_Ej1.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo20_ej1;
+
+/**
+ *
+ * @author capacita_mecon
+ */
+public class Ejemplo20_Ej1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            Ejemplo20_Ej1.verificarRango(101, 10);
+        }
+        catch(fueraDeRangoExcepcion e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        try
+        {
+            Ejemplo20_Ej1.verificarRango(101, 0);
+        }
+        catch(fueraDeRangoExcepcion e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        try
+        {
+            Ejemplo20_Ej1.verificarRango(55, 11);
+        }
+        catch(fueraDeRangoExcepcion e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void verificarRango(int par1, int par2) throws fueraDeRangoExcepcion
+    {
+        float resultado = 0.0F;
+        
+        if(!(par1 < 100 && par2 > 9))
+        {
+            throw new fueraDeRangoExcepcion("ERROR. El parámetro 1 debe ser menor a 100 y el parámetro 2 mayor a 9");
+        }
+        else
+        {
+            resultado = par1 / (float)par2;
+            System.out.print(par1 + " / " + par2 + " = ");
+            System.out.print(resultado);
+        }
+    }
+    
+}
---
+++ Ejemplo20_Ej1/src/ejemplo20_ej1/fueraDeRangoExcepcion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo20_ej1;
+
+/**
+ *
+ * @author capacita_mecon
+ */
+public class fueraDeRangoExcepcion extends Exception
+{
+    public fueraDeRangoExcepcion(String mensaje)
+    {
+        super(mensaje);
+    }
+}
